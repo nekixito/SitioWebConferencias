6bd0917d7fa7e5c74ba1f4a6f2a479f2a5467163
Miguel Nequiz <nekixito@gmail.com>
2021-02-19 09:26:46 -0600

    Menú fijo utilizando el scroll

--- js/main.js
+++ js/main.js
@@ -161,6 +161,27 @@ $(function() {
     //Lettering
     $('.nombre-sitio').lettering();
 
+    //Menu fijo
+
+    var windowHeight = $(window).height();
+    var barraAltura = $('.barra').innerHeight();
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > windowHeight) {
+            $('.barra').addClass('fixed');
+            $('body').css({
+                'margin-top': barraAltura + 'px'
+            });
+        } else {
+            $('.barra').removeClass('fixed');
+            $('body').css({
+                'margin-top': '0px'
+            });
+        }
+
+    });
+
     //Programa de conferencias
     $('.programa-evento .info-curso:first').show();
     $('.menu-programa a:first').addClass('activo');
